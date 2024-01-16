eff7e05756ffa4e1b1e791aa7200745e2d3056f1
rootminus0x1 <rootminus0x1@gmail.com>
2024-01-16 01:31:42 +0000

    broke out some config file parsing bug logs for missing signers - fix next

--- src/delve.ts
+++ src/delve.ts
@@ -101,6 +101,7 @@ export const calculateMeasures = async (graph: Graph): Promise<Measurements> => 
     let count = 0;
 
     const sortedNodes = sort(graph.nodes, (v) => v.name);
+    console.log(`Nodes: ${sortedNodes.length}`);
     for (const [address, node] of sortedNodes) {
         let values: Measurement[] = []; // values for this graph node
         const measures = graph.measures.get(address);
@@ -113,7 +114,6 @@ export const calculateMeasures = async (graph: Graph): Promise<Measurements> => 
                     values.push({ name: measure.name, type: measure.type, error: e.message });
                 }
             }
-            count++;
         }
         const measuresOnAddress = graph.measuresOnAddress.get(address);
         if (measuresOnAddress && measuresOnAddress.length > 0) {
@@ -135,7 +135,6 @@ export const calculateMeasures = async (graph: Graph): Promise<Measurements> => 
                             error: e.message,
                         });
                     }
-                    count++;
                 }
             }
         }
@@ -146,6 +145,7 @@ export const calculateMeasures = async (graph: Graph): Promise<Measurements> => 
                 contract: await node.contractNamish(),
                 measurements: values,
             });
+            count += values.length;
         }
     }
     console.log(`Measurements: ${count}`);
@@ -243,9 +243,24 @@ export const calculateDeltaMeasures = (
     return results;
 };
 
+const parseArg = (configArg: any, users?: any, contracts?: any): string | bigint => {
+    let arg: any;
+    if (typeof configArg === 'bigint') {
+        arg = configArg;
+    } else if (typeof configArg === 'string') {
+        // contract or user or address or string or number
+        const match = configArg.match(/^\s*(\d+)\s*(\w+)\s*$/);
+        if (match && match.length === 3) arg = parseUnits(match[1], match[2]);
+        else if (users[configArg]) arg = users[configArg].address;
+        else if (contracts[configArg]) arg = contracts[configArg].address;
+    } else if (typeof configArg === 'number') arg = BigInt(configArg);
+    else arg = 0n;
+    return arg;
+};
+
 ////////////////////////////////////////////////////////////////////////
 // delve
-export const delve = async (config: Config, graph: Graph, blockchain: Blockchain): Promise<void> => {
+export const delve = async (config: Config, dugGraph: Graph, blockchain: Blockchain): Promise<void> => {
     // formatting of output
     const formatFromConfig = (address: any): any => {
         // "string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function"
@@ -290,9 +305,11 @@ export const delve = async (config: Config, graph: Graph, blockchain: Blockchain
     // TODO: allow additional actions this to be passed in for user defined actions?
 
     let baseMeasurements: Measurements;
-    for (const configAction of [nullAction, ...config.actions]) {
+    for (const configAction of [nullAction, ...(config.actions ? config.actions : [])]) {
         // start with a fresh blockchain
         blockchain.reset();
+        const graph = lodash.cloneDeep(dugGraph);
+        console.log(`graph: ${dugGraph.nodes.size} => ${graph.nodes.size}`);
 
         let contracts: any = {};
         let users: any = {};
@@ -307,45 +324,56 @@ export const delve = async (config: Config, graph: Graph, blockchain: Blockchain
             }
         }
         // add in the users from the config
-        for (const user of config.users) {
-            const signer = await blockchain.getSigner(user.name);
-            users[user.name] = signer; // to be used in actions
-            // add them to the graph, too
-            graph.nodes.set(signer.address, Object.assign({ name: user.name, signer: signer }, digOne(signer.address)));
-        }
-
-        // preconditions of actions
-        // TODO: under config
-        // get some stETH for some users and let market use it
-        const stEthWhale = await ethers.getImpersonatedSigner('0x95ed9BC02Be94C17392fE819A93dC57E73E1222E');
-        for (const user of [users.fMinter, users.xMinter]) {
-            if (!(await contracts.Lido.connect(stEthWhale).transfer(user.address, parseEther('10')))) {
-                throw Error('could not get enough stETH, find another whale');
+        if (config.users) {
+            for (const user of config.users) {
+                const signer = await blockchain.getSigner(user.name);
+                users[user.name] = signer; // to be used in actions
+                // add them to the graph, too
+                console.log(
+                    `about to add user ${user.name}, ${signer.address}, is that address free? ${JSON.stringify(
+                        graph.nodes.get(signer.address),
+                    )}`,
+                );
+                graph.nodes.set(
+                    signer.address,
+                    Object.assign({ name: user.name, signer: signer }, digOne(signer.address)),
+                );
+                console.log(`after adding user ${user.name}, ${signer.address}, graph: ${graph.nodes.size}`);
+                if (user.wallet) {
+                    for (const holding of user.wallet) {
+                        // fill the wallet
+                        // TODO: create a whales file that hands out dosh
+                        const stEthWhale = await ethers.getImpersonatedSigner(
+                            '0x95ed9BC02Be94C17392fE819A93dC57E73E1222E',
+                        );
+                        if (
+                            !(await contracts[holding.contract]
+                                .connect(stEthWhale)
+                                .transfer(users[user.name].address, parseArg(holding.amount)))
+                        ) {
+                            throw Error('could not get enough stETH, find another whale');
+                        }
+                        // find all the contracts this user interacts with and allow them to spend there
+                        if (config.actions) {
+                            for (const contract of config.actions
+                                .filter((a) => a.user && a.user === user.name)
+                                .map((a) => a.contract)) {
+                                // allow the wallet to be spent
+                                await contracts[holding.contract]
+                                    .connect(users[user.name])
+                                    .approve(contracts[contract].address, MaxUint256);
+                            }
+                        }
+                    }
+                }
             }
-            await contracts.Lido.connect(user).approve(contracts.Market.address, MaxUint256);
         }
-
         const actionName = `${configAction.contract}-${configAction.function}(${JSON.stringify(configAction.args)
             .replace(/[/\\:*?"<>|]/g, '')
             .replace(/\s+/g, '_')}-${configAction.user}`;
 
         const action: ActionFunction = async () => {
-            // process the args
-            let args: (string | bigint)[] = [];
-            for (const configArg of configAction.args) {
-                let arg: any;
-                if (typeof configArg === 'bigint') {
-                    arg = configArg;
-                } else if (typeof configArg === 'string') {
-                    // contract or user or address or string or number
-                    const match = configArg.match(/^\s*(\d+)\s*(\w+)\s*$/);
-                    if (match && match.length === 3) arg = parseUnits(match[1], match[2]);
-                    else if (users[configArg]) arg = users[configArg].address;
-                    else if (contracts[configArg]) arg = contracts[configArg].address;
-                } else if (typeof configArg === 'number') arg = BigInt(configArg);
-                else arg = 0n;
-                args.push(arg);
-            }
+            const args = configAction.args.map((a) => parseArg(a, users, contracts));
             return contracts[configAction.contract].connect(users[configAction.user])[configAction.function](...args);
         };
 
